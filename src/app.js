ee9ea65309a45e78e7888a8972f93dc4156cd21e
HuluWZ <hulunlante.w@gmail.com>
2024-08-03 11:19:01 +0300

    chore: Update MongoDB URIs, add service worker and index.html

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(express.json());
 
 if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
 
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
+});
+
+app.get("*", function response(req, res) {
+  res.sendFile("./public/index.html", { root: __dirname });
+});
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
